fa781237c68dc5d309493e441d90ceac8c6a045e
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-10-05 12:10:21 -0300

    Fim da aula do módulo 11, inicio do exercicio

--- sql_impressionador/modulo_11.sql
+++ sql_impressionador/modulo_11.sql
@@ -1,12 +1,33 @@
----#############
---Modulo 11
+--#############
+-- Modulo 11 - Manipuladno textos e datas no SQL
 
-SELECT	LEN('Teste de função    '),				-- Não conta os espaços
-		DATALENGTH('Teste de função      ')		-- Conta os espaços
+-- Conteúdo:
+/*
+- LEN e DATALENGHT
+- CONCAT
+- LEFT e RIGHT
+- REPLACE, TRANSLATE e STUFF
+- UPPER e LOWER
+- FORMAT
+- CHARINDEX
+- SUBSTRING
+- TRIM, LTRIM e RTRIM
+- YEAR, MONTH e DAY
+- DATEFROMPARTS
+- GETDATE, SYSDATETIME
+- DATEPART
+- DATENAME
+- DATEADD
+- DATEDIFF
+*/
 
---############
--- Concat
--- Queremos visualizar o nome completo de todos os clientes:
+-- ##########
+-- LEN e DATALENGHT
+SELECT	LEN('Teste de função    ') AS 'LEN',				-- Não conta os espaços
+		DATALENGTH('Teste de função      ') AS 'DATALENGHT'		-- Conta os espaços
+
+--##################
+-- CONCAT >> Juntar 2 ou mais textos.
 
 SELECT
 	FirstName AS 'Nome',
@@ -15,8 +36,10 @@ SELECT
 	EmailAddress AS 'E-mail'
 FROM
 	DimCustomer
+
 --##################
---Dividir uma coluna me duas
+-- LEFT e RIGHT
+-- Extrai uma determinada quantidade de caracteres de um texto
 
 SELECT LEFT('Product1111111',7)
 SELECT RIGHT('Product1111111',7)
@@ -32,7 +55,7 @@ FROM
 	DimProduct
 
 --##################
---Replace
+--REPLACE >> Subistituir um texto específico por outro
 
 SELECT REPLACE('O EXCEL é o melhor','EXCEL','SQL')
 
@@ -55,16 +78,19 @@ SELECT
 	REPLACE(REPLACE(Gender,'M','Masculino'),'F','Feminino')
 FROM 
 	DimCustomer
+
 --####################
---Translate
+--TRANSLATE >> Subistitui uma ûm vetor de caracteres
 
 SELECT TRANSLATE('119.279.124.07','...','---')
+
 --####################
---Stuff
+--STUFF >> Substitui um range de caracteres
 SELECT STUFF('119.279.124.07',1,3,'---')
+
 --####################
---UPPER
---LOWER
+--UPPER >> Miscucula
+--LOWER >> minuscula
 
 SELECT FirstName as 'Nome', UPPER(FirstName) as 'NOME', LOWER(FirstName) as 'nome' , EmailAddress as 'e-mail'  FROM DimCustomer
 
@@ -85,13 +111,56 @@ SELECT FORMAT(5123, 'C')
 SELECT FORMAT(CAST('23/04/2020' AS datetime),'dd/MMM/yy')
 
 -- ##################
--- Charindex:
--- Substring:
+-- CHARINDEX >> index de início de um caracter
+-- SUBSTRING >> Selecionar um trexo pela posição de um string
 
 SELECT CHARINDEX('Gui', 'Emerson Guilherme')
 
 SELECT SUBSTRING('Raquel Moreno', 8, 6) as 'Sobrenome'
 
--- ##############
+DECLARE @varnome VARCHAR(100)
+SET @varnome = 'Luis Dias'
+SELECT SUBSTRING(@varnome, CHARINDEX(' ',@varnome) +1 , 6) as 'Sobrenome'
 
---51 block 17
+-- ##################
+-- - TRIM, LTRIM e RTRIM
+
+DECLARE @Varcodigo VARCHAR(50)
+SET @Varcodigo = '    ABC123 '
+
+SELECT 
+	TRIM(@Varcodigo) as 'TRIM',
+	LTRIM(@Varcodigo) as 'LTRIM',
+	RTRIM(@Varcodigo) as 'RTRIM'
+
+SELECT 
+	DATALENGTH(TRIM(@Varcodigo)) as 'TRIM',
+	DATALENGTH(LTRIM(@Varcodigo)) as 'LTRIM',
+	DATALENGTH(RTRIM(@Varcodigo)) as 'RTRIM'
+
+-- ##################
+-- - YEAR, MONTH e DAY
+
+DECLARE @vardata DATETIME
+SET @vardata = '18/05/2020'
+
+SELECT
+	DAY(@vardata) as 'Dia',
+	MONTH(@vardata) as 'Mês',
+	YEAR(@vardata) as 'Ano'
+
+--####################
+-- DATEFROMPARTS
+
+DECLARE @varDia INT, @varMes INT, @varAno INT
+SET @varDia = 15
+SET @varMes = 10
+SET @varAno = 2017
+
+SELECT
+	DATEFROMPARTS(@varAno, @varMes, @varDia) AS 'Data'
+
+-- ####################
+-- GETDATE
+-- SYSDATETIME
+-- DATEPART
